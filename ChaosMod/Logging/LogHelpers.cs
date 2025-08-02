3bfd7add70bbfb15292cd61fe7c42c9a1fb4ea0b
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-08-02 17:05:39 -0600

    Added log helpers

---
+++ ChaosMod/Logging/LogHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrootLuips.ChaosMod.Logging;
+
+public delegate void StatusCallback(List<string> issues, bool success);
+
+internal class LogHelpers
+{
+}
