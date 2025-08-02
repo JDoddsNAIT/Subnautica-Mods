bd71c3083c57187220cdcfd38ba990884785de8e
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-08-02 13:43:10 -0600

    Created project files

---
+++ ChaosMod/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+
+namespace ChaosMod;
+[BepInPlugin(GUID, NAME, VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+	public const string GUID = "com.github.frootluips.ChaosMod";
+	public const string NAME = "FrootLuips' Chaos Mod";
+	public const string VERSION = "1.0.0";
+
+	private void Awake()
+	{
+		
+	}
+}
