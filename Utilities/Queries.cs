54da10534f880b2aec0aba0b408e3742ceaca936
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-08-09 21:27:06 -0600

    Added documentation for queries

--- Utilities/Queries.cs
+++ Utilities/Queries.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using static UnityEngine.UIElements.UIRAtlasManager;
 
 namespace FrootLuips.Subnautica;
 /// <summary>
@@ -10,6 +9,14 @@ public static class Queries
 {
 	public static bool NotNull<T>(T? value) => value != null;
 
+	/// <summary>
+	/// Converts a <typeparamref name="T1"/> collection into a <typeparamref name="T2"/> array.
+	/// </summary>
+	/// <typeparam name="T1"></typeparam>
+	/// <typeparam name="T2"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="selector"></param>
+	/// <param name="destination"></param>
 	public static void Convert<T1, T2>(IReadOnlyList<T1> source, Func<T1, T2> selector, ref T2[] destination)
 	{
 		int length = source.Count;
@@ -22,6 +29,14 @@ public static class Queries
 		}
 	}
 
+	/// <summary>
+	/// Converts a <typeparamref name="T1"/> collection into a list of <typeparamref name="T2"/>s.
+	/// </summary>
+	/// <typeparam name="T1"></typeparam>
+	/// <typeparam name="T2"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="selector"></param>
+	/// <param name="destination"></param>
 	public static void Convert<T1, T2>(IReadOnlyList<T1> source, Func<T1, T2> selector, List<T2> destination)
 	{
 		destination.Capacity = source.Count;
@@ -35,23 +50,47 @@ public static class Queries
 		}
 	}
 
-	public static void Copy<T>(T[] source, ref T[] destination)
+	/// <summary>
+	/// Copies the elements from the <paramref name="source"/> into the <paramref name="destination"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="destination"></param>
+	public static void Copy<T>(IReadOnlyList<T> source, ref T[] destination)
 	{
-		Queries.Convert<T, T>(source, selector: static obj => obj, ref destination);
+		Convert(source, selector: static obj => obj, ref destination);
 	}
 
-	public static void Copy<T>(List<T> source, List<T> destination)
+	/// <summary>
+	/// Copies the elements from the <paramref name="source"/> into the <paramref name="destination"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="destination"></param>
+	public static void Copy<T>(IReadOnlyList<T> source, List<T> destination)
 	{
-		Queries.Convert<T, T>(source, selector: static obj => obj, destination);
+		Convert(source, selector: static obj => obj, destination);
 	}
 
+	/// <summary>
+	/// Filters out all elements of the <paramref name="array"/> that do not pass the <paramref name="filter"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
+	/// <param name="filter"></param>
 	public static void Filter<T>(ref T[] array, Predicate<T> filter)
 	{
 		List<T> result = new(array);
-		Queries.Filter(result, filter);
-		array = result.ToArray();
+		Filter(result, filter);
+		Copy(result, ref array);
 	}
 
+	/// <summary>
+	/// Filters out all elements of the <paramref name="list"/> that do not pass the <paramref name="filter"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="filter"></param>
 	public static void Filter<T>(List<T> list, Predicate<T> filter)
 	{
 		for (int i = list.Count - 1; i >= 0; i--)
@@ -61,24 +100,29 @@ public static class Queries
 		}
 	}
 
+	/// <summary>
+	/// Removes all duplicate elements from the <paramref name="array"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
 	public static void FilterDuplicates<T>(ref T[] array)
 	{
 		var duplicates = new List<T>(capacity: array.Length);
-		bool isUnique(T value)
+		for (int i = 0; i < array.Length; i++)
 		{
-			if (duplicates.Contains(value))
-			{
-				return false;
-			}
+			if (!duplicates.Contains(array[i]))
+				duplicates.Add(array[i]);
 			else
-			{
-				duplicates.Add(value);
-				return true;
-			}
+				continue;
 		}
-		Queries.Filter<T>(ref array, filter: isUnique);
+		Copy(duplicates, ref array);
 	}
 
+	/// <summary>
+	/// Removes all duplicate elements from the <paramref name="list"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
 	public static void FilterDuplicates<T>(List<T> list)
 	{
 		var duplicates = new List<T>(capacity: list.Count);
@@ -94,6 +138,26 @@ public static class Queries
 				return true;
 			}
 		}
-		Queries.Filter<T>(list, filter: isUnique);
+		Filter<T>(list, filter: isUnique);
+	}
+
+	/// <summary>
+	/// Filters <see langword="null"/> values out of the <paramref name="array"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
+	public static void FilterNulls<T>(ref T[] array)
+	{
+		Filter(ref array, filter: NotNull);
+	}
+
+	/// <summary>
+	/// Filters <see langword="null"/> values out of the <paramref name="list"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	public static void FilterNulls<T>(List<T> list)
+	{
+		Filter(list, filter: NotNull);
 	}
 }
