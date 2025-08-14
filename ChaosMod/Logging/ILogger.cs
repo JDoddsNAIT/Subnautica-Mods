0fa5cee9bdb012e6ef2c894307a60025af84ff34
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-08-14 13:21:31 -0600

    .

--- ChaosMod/Logging/ILogger.cs
+++ ChaosMod/Logging/ILogger.cs
@@ -10,3 +10,31 @@ public interface ILogger
 	void LogError(string message);
 	void LogFatal(string message);
 }
+
+public static class LoggerExtensions
+{
+	public static void Log(this ILogger logger, LogLevel level, string message)
+	{
+		switch (level.GetHighestLevel())
+		{
+			case LogLevel.Fatal:
+				logger.LogFatal(message);
+				break;
+			case LogLevel.Error:
+				logger.LogError(message);
+				break;
+			case LogLevel.Warning:
+				logger.LogWarning(message);
+				break;
+			case LogLevel.Message:
+				logger.LogMessage(message);
+				break;
+			case LogLevel.Info:
+				logger.LogInfo(message);
+				break;
+			default:
+				logger.LogDebug(message);
+				break;
+		}
+	}
+}
