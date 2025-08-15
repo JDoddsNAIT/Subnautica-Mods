4a7e9cd83545a8d3284268a878e5e2602e1e3633
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-08-15 12:06:53 -0600

    Updated validation

--- Utilities/Validation.Result.cs
+++ Utilities/Validation.Result.cs
@@ -5,14 +5,34 @@ namespace FrootLuips.Subnautica;
 public static partial class Validation
 {
 	/// <summary>
-	/// The represents the result of a validation process.
+	/// Represents the result of a validation process.
 	/// </summary>
 	/// <param name="Success">Whether validation succeeded.</param>
 	/// <param name="Errors">List of issues.</param>
-	public readonly record struct Result(bool Success, List<Exception> Errors)
+	public readonly record struct Result(bool Success, IReadOnlyList<Exception> Errors)
 	{
 		/// <param name="obj"></param>
 		public static implicit operator bool(Result obj) => obj.Success;
+
+		/// <summary>
+		/// Throws an <see cref="AggregateException"/> of all <see cref="Errors"/> if <see cref="Success"/> is <see langword="false"/>.
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="AggregateException"/>
+		public bool ThrowIfFailed() => Success ? true : throw Errors.ToAggregate();
+	}
+
+	/// <summary>
+	/// Represents the result of a validation process for a <typeparamref name="T"/> object.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="Value">The object that was validated.</param>
+	/// <param name="Success">Whether validation succeeded.</param>
+	/// <param name="Errors">List of issues.</param>
+	public readonly record struct Result<T>(T Value, bool Success, IReadOnlyList<Exception> Errors)
+	{
+		/// <param name="obj"></param>
+		public static implicit operator bool(Result<T> obj) => obj.Success;
 
 		/// <summary>
 		/// Throws an <see cref="AggregateException"/> of all <see cref="Errors"/> if <see cref="Success"/> is <see langword="false"/>.
--- Utilities/Validation.cs
+++ Utilities/Validation.cs
@@ -70,6 +70,37 @@ public static partial class Validation
 	}
 
 	/// <summary>
+	/// Validates the given <paramref name="object"/>, iterating over the <paramref name="validator"/> then returning the result.
+	/// </summary>
+	/// <remarks>
+	/// Successful validation is determined by whether any issues were found, or the result of <paramref name="callback"/> if one is provided.
+	/// </remarks>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="object"></param>
+	/// <param name="validator"></param>
+	/// <param name="callback"></param>
+	/// <returns>Whether validation succeeded or not, along with any issues that may have occurred.</returns>
+	public static Result<T> ValidateObject<T>(T @object,
+		IEnumerator<Exception> validator,
+		Func<T, bool>? callback = null)
+	{
+		var errors = new List<Exception>();
+		callback ??= o => o != null && errors.Count == 0;
+		try
+		{
+			while (validator.MoveNext())
+			{
+				errors.Add(validator.Current);
+			}
+		}
+		catch (Exception ex)
+		{
+			errors.Add(ex);
+		}
+		return new Result<T>(@object, callback(@object), errors);
+	}
+
+	/// <summary>
 	/// Creates an <see cref="AggregateException"/> from a collection of <paramref name="exceptions"/>.
 	/// </summary>
 	/// <param name="exceptions"></param>
