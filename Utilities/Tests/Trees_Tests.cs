1d07ce5f3ec2b1775c0eb474056bcfff7930e01b
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-08-17 13:28:37 -0600

    Created testing class for Trees

---
+++ Utilities/Tests/Trees_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using FrootLuips.Subnautica.Trees;
+
+namespace FrootLuips.Subnautica.Tests;
+internal class Trees_Tests : ITestContainer
+{
+	public IEnumerator<TestResult> GetResults()
+	{
+		string group = nameof(Trees_Tests);
+		yield return TestResult.Assert(nameof(Enumerate_BFS), Enumerate_BFS, group);
+	}
+
+	private bool Enumerate_BFS(out string message)
+	{
+		message = "";
+		return false;
+	}
+}
